7d12aa18820cdb23c0f06bf49a6a364754a0a569
Richard-Einsteine <karasirarichard250@gmail.com>
2024-07-25 18:40:10 +0200

    Colorscheme configuration Different machine hostnames can have different default colorschemes

--- .config/nvim/lua/config/autocmd.lua
+++ .config/nvim/lua/config/autocmd.lua
@@ -1,9 +1,16 @@
 local autocmd = vim.api.nvim_create_autocmd
+local hostname = vim.fn.system("hostname"):gsub("\n$", "")
 
 -- vim.highlight.on_yank()
-autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
+
+if hostname == "kara" then
+	vim.cmd.colorscheme("bamboo")
+else
+   vim.cmd.colorscheme("sonokai")
+end
--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -1,4 +1,5 @@
 return {
+   "sainnhe/sonokai",
 	{
 		"EdenEast/nightfox.nvim",
 		lazy = false,
@@ -8,34 +9,30 @@ return {
 					colorblind = {
 						enable = true,
 						severity = {
-							protan = 0.3,     -- red
-							deutan = 0.6,     -- green
+							protan = 0.3, -- red
+							deutan = 0.6, -- green
 							-- tritan = 0.6,     -- blue
 						},
 					},
 				},
 			})
-
-			-- vim.cmd.colorscheme("dayfox")
 		end,
 	},
 	{
 		"ribru17/bamboo.nvim",
 		lazy = false,
-		config = function()
-			vim.cmd.colorscheme("bamboo")
-		end,
 	},
 	{
 		"scottmckendry/cyberdream.nvim",
 		lazy = false,
-		opts = {
-			theme = {
-				variant = "auto",
-			},
-		},
 		config = function()
-			-- vim.cmd.colorscheme("cyberdream")
+			require("cyberdream").setup({
+				options = {
+					theme = {
+						variant = "auto",
+					},
+				},
+			})
 		end,
 	},
 }
