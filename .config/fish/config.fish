1f27607f5277eedf21604258a9caa2bf93ef350f
Richard-Einsteine <karasirarichard250@gmail.com>
2024-07-27 22:40:41 +0200

    Fish Configuration has been added    - some functions    - a config file

---
+++ .config/fish/config.fish
@@ -0,0 +1,6 @@
+set -Ux fish_greeting
+
+function multicd
+    echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
+end
+abbr --add dotdot --regex '^\.\.+$' --function multicd
---
+++ .config/fish/functions/vidtitle.fish
@@ -0,0 +1,19 @@
+function vidtitle
+   if test (count $argv) -ne 1
+      echo "usage: vidtitle <mkv|mp4>"
+      return 1
+   else if not string match -qr "mkv|mp4" $argv[1]
+      echo '"mkv" or "mp4" extensions only'
+      return 1
+   end
+
+   for file in (fdfind -e $argv[1])
+      echo $file
+      if test $argv[1] = "mkv"
+         mkvpropedit $file -e info -s title=(basename $file)
+      else if test $argv[1] = "mp4"
+         exiftool -All= "$file"
+         rm -rf "$file"_original
+      end
+   end
+end
