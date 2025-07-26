c99950f9b8bdf166998b3f3b047883ebd0d92b56
bladevalor <karasirarichard250@gmail.com>
2025-07-26 14:15:57 +0300

    remove the nvim config I had customized and migrated to using lazyVim config framework

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,20 @@
 set -Ux fish_greeting
+set -Ux SUDO_EDITOR nvim
 
 function multicd
     echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
 end
 abbr --add dotdot --regex '^\.\.+$' --function multicd
+
+set -Ux DEEPSEEK_API_KEY sk-b73defc36a794152948038c96ae5e1ae
+
+set -Ux ARDMK_DIR "/opt/Arduino-Makefile"
+set -Ux ARDUINO_DIR ""
+set -Ux AVR_TOOLS_DIR ""
+
+function x
+   xdg-open $argv
+end
 
 function ks
    exa -a $argv
@@ -13,13 +24,22 @@ function nv
    nvim $argv
 end
 
+function lv
+   env NVIM_APPNAME=nvim-lazy nvim $argv
+end
+
+function av
+   env NVIM_APPNAME=nvim-astro nvim $argv
+end
+
+function xo
+   xdg-open $argv
+end
+
 function reload
    source ~/.config/fish/config.fish
 end
 
-function x
-   xdg-open $argv
-end
-
 fzf --fish | source
+zoxide init fish | source
 starship init fish | source
