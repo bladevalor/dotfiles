db7203d7b6a1d51afad1c6a8eff1b134dacb1a07
bladevalor <karasirarichard250@gmail.com>
2025-11-18 02:56:39 +0200

    emacs' like "compile mode" for nvim

---
+++ .config/nvim/lua/plugins/compile-mode.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "ej-shafran/compile-mode.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      ---@type CompileModeOpts
+      vim.g.compile_mode = {
+        -- to add ANSI escape code support, add:
+        -- baleia_setup = true,
+
+        -- to make `:Compile` replace special characters (e.g. `%`) in
+        -- the command (and behave more like `:!`), add:
+        -- bang_expansion = true,
+      }
+    end,
+  },
+}
