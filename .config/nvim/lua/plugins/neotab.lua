8fe316a7fd2d022134c272e7f27ae9dc04fb6a2c
bladevalor <karasirarichard250@gmail.com>
2025-11-18 02:55:14 +0200

    smarter tab completions

--- .config/nvim/lua/plugins/neotab.lua
+++ .config/nvim/lua/plugins/neotab.lua
@@ -6,4 +6,22 @@ return {
       -- configuration goes here
     },
   },
+  {
+    "L3MON4D3/LuaSnip",
+    build = "make install_jsregexp",
+    dependencies = { "neotab.nvim" },
+    keys = {
+      {
+        "<Tab>",
+        function()
+          return require("luasnip").jumpable(1) --
+              and "<Plug>luasnip-jump-next"
+            or "<Plug>(neotab-out)"
+        end,
+        expr = true,
+        silent = true,
+        mode = "i",
+      },
+    },
+  },
 }
