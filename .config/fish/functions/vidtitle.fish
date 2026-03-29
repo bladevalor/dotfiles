9af48ccb7d3c235f286e63b1073f548fc30667ea
bladevalor <karasirarichard250@gmail.com>
2026-03-29 12:17:23 +0200

    reimplementation of "vidtitle". i think its cleaner and more robust

--- .config/fish/functions/vidtitle.fish
+++ .config/fish/functions/vidtitle.fish
@@ -1,19 +1,43 @@
 function vidtitle
-   if test (count $argv) -ne 1
-      echo "usage: vidtitle <mkv|mp4>"
-      return 1
-   else if not string match -qr "mkv|mp4" $argv[1]
-      echo '"mkv" or "mp4" extensions only'
-      return 1
-   end
+    # Dependency Check
+    set -l deps mkvpropedit exiftool fd
+    for d in $deps
+        if not type -q $d
+            kdialog --title "System Error" --error "Required command '$d' not found."
+            return 1
+        end
+    end
 
-   for file in (fd -e $argv[1])
-      echo $file
-      if test $argv[1] = "mkv"
-         mkvpropedit $file -e info -s title=(basename $file)
-      else if test $argv[1] = "mp4"
-         exiftool -All= "$file"
-         rm -rf "$file"_original
-      end
-   end
+    for target in $argv
+        if test -d "$target"
+            for file in (fd -t f . "$target")
+                __process_file "$file"
+            end
+
+        else if test -f "$target"
+            __process_file "$target"
+
+        else
+            kdialog --error "'$target' is not a valid target."
+            continue
+        end
+    end
+
+    kdialog --title "Vid Title" --passivepopup "Process finished." 3
+end
+
+# Internal helper function
+function __process_file
+    set -l file "$argv[1]"
+
+    switch (string lower (path extension "$file"))
+        case .mkv
+            mkvpropedit "$file" -e info -s title=(path basename -E "$file")
+        case .mp4 .mov .m4v
+            exiftool -overwrite_original -Title=(path basename -E "$file") "$file"
+            # mp4edit_title "$file" &
+        case *
+            # Ignore other files
+            return 0
+    end
 end
