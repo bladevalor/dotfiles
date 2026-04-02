68f9c6a9de56214cc7c9f41bd143050448fc9282
bladevalor <karasirarichard250@gmail.com>
2026-04-02 12:32:49 +0200

    plugins now trigers/loads in flutter/dart projects (pubspec.yaml)

--- .config/nvim/lua/plugins/flutter.lua
+++ .config/nvim/lua/plugins/flutter.lua
@@ -1,18 +1,48 @@
 return {
   {
     "nvim-flutter/flutter-tools.nvim",
-    lazy = false,
+    lazy = true,
+
+    init = function()
+      local loaded = false
+      local function check()
+        local cwd = vim.uv.cwd()
+        if vim.fn.filereadable(cwd .. "/pubspec.yaml") == 1 then
+          require("lazy").load({ plugins = { "flutter-tools.nvim" } })
+          loaded = true
+        end
+      end
+      check()
+      vim.api.nvim_create_autocmd("DirChanged", {
+        callback = function()
+          if not loaded then
+            check()
+          end
+        end,
+      })
+    end,
+
     dependencies = {
       "nvim-lua/plenary.nvim",
       "stevearc/dressing.nvim", -- optional for vim.ui.select
     },
-    config = true,
-    keys = {
-      {
-        "<leader>tl",
-        "<cmd>FlutterLogToggle<cr>",
-        desc = "Toggle flutter logs",
-      },
-    },
+
+    config = function()
+      require("flutter-tools").setup({})
+
+      local which = require("which-key")
+      which.add({
+        {
+          "<leader>fl",
+          group = "Flutter/Dart",
+          icon = " ",
+        },
+        {
+          "<leader>fll",
+          "<cmd>FlutterLogToggle<cr>",
+          desc = "Toggle flutter logs",
+        },
+      })
+    end,
   },
 }
