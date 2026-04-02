cb81bb52e5516a4ab1889216e912718b370664cc
bladevalor <karasirarichard250@gmail.com>
2026-04-02 12:31:54 +0200

    "uv.nvim" now trigers/loads when an pyproject.toml is present

--- .config/nvim/lua/plugins/python_uv.lua
+++ .config/nvim/lua/plugins/python_uv.lua
@@ -1,6 +1,26 @@
 return {
   {
     "benomahony/uv.nvim",
+    lazy = true,
+
+    init = function()
+      local loaded = false
+      local function check()
+        local cwd = vim.uv.cwd()
+        if vim.fn.filereadable(cwd .. "/pyproject.toml") == 1 then
+          require("lazy").load({ plugins = { "uv.nvim" } })
+          loaded = true
+        end
+      end
+      check()
+      vim.api.nvim_create_autocmd("DirChanged", {
+        callback = function()
+          if not loaded then
+            check()
+          end
+        end,
+      })
+    end,
 
     opts = {
       picker_integration = true,
